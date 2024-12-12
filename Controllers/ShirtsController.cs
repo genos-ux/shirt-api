eb0c93be8e1637abe1e8ddb16bca312daef8da4b
genos-ux <gabriel.kyeremateng.gk@gmail.com>
2024-12-12 19:32:38 +0000

    Created the various endpoints in the ShirtsController

--- Controllers/ShirtsController.cs
+++ Controllers/ShirtsController.cs
@@ -10,6 +10,34 @@ namespace shirt_api.Controllers
     [Route("api/[controller]")]
     public class ShirtsController : ControllerBase
     {
-        
+        [HttpGet]
+        public string GetShirts()
+        {
+            return "Reading all shirts.";
+        }
+
+        [HttpGet("{id}")]
+        public string GetShirtById(int id)
+        {
+            return $"Reading shirt with ID: {id}";
+        }
+
+        [HttpPost]
+        public string CreateShirt()
+        {
+            return "Creating a shirt";
+        }
+
+        [HttpPut("{id}")]
+        public string UpdateShirt(int id)
+        {
+            return $"Updating shirt with ID: {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string DeleteShirt(int id)
+        {
+            return $"Deleting shirt with ID: {id}";
+        }
     }
 }
