59d22471c1fa9bf2ccd58b3d0a9543d6412dff24
genos-ux <gabriel.kyeremateng.gk@gmail.com>
2024-12-19 02:21:13 +0000

    Created an in memory repository

--- Controllers/ShirtsController.cs
+++ Controllers/ShirtsController.cs
@@ -13,13 +13,7 @@ namespace shirt_api.Controllers
     public class ShirtsController : ControllerBase
     {
 
-        private List<Shirt> shirts = new List<Shirt>
-        {
-            new Shirt { ShirtId = 1, Brand = "My Brand", Color = "Blue", Gender = "Men", Price = 30, Size = 10},
-            new Shirt { ShirtId = 2, Brand = "My Brand", Color = "Black", Gender = "Men", Price = 35, Size = 12},
-            new Shirt { ShirtId = 3, Brand = "Your Brand", Color = "Pink", Gender = "Women", Price = 28, Size = 8},
-            new Shirt { ShirtId = 4, Brand = "Your Brand", Color = "Yellow", Gender = "Women", Price = 30, Size = 9}
-        };
+        
 
         [HttpGet]
         public IActionResult GetShirts()
