87e7b2ff1a047b978ae407593d9257b9f4fb2d6d
genos-ux <gabriel.kyeremateng.gk@gmail.com>
2024-12-20 20:22:05 +0000

    Created an action filter for the CreateShirt endpoint parameter

--- obj/Debug/net8.0/shirt-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/shirt-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("shirt-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53f01bde5e054147c3fa0bea24314c9ceff1387d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9be8e47fce415326e4c5c5e7425d8be791db8cc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("shirt-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("shirt-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
